b0b3e7e035898599a2c138c81ff883db1a33691d
Sherly1001 <Sherly1001@users.noreply.github.com>
2023-06-20 22:52:02 +0700

    chore: fix icons import

--- frontend/src/custom-icons/index.ts
+++ frontend/src/custom-icons/index.ts
@@ -1,3 +1,6 @@
+export * from './caret-down';
+export * from './caret-none';
+export * from './caret-up';
 export * from './dashboard';
 export * from './deep-learing';
 export * from './right-rect';
